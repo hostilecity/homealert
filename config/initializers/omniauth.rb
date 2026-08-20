59022923508977236f7bfb274875af3c53cb641e
ryaan-anthony <rtulino@gmail.com>
2026-08-20 14:46:05 -0400

    add guard to omniauth initializer

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,11 +1,10 @@
-google_client_id     = Rails.env.production? ? ENV.fetch("GOOGLE_CLIENT_ID") : ENV.fetch("GOOGLE_CLIENT_ID", "")
-google_client_secret = Rails.env.production? ? ENV.fetch("GOOGLE_CLIENT_SECRET") : ENV.fetch("GOOGLE_CLIENT_SECRET", "")
+return if Rails.env.test?
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2,
-    google_client_id,
-    google_client_secret,
-    scope: "email,profile"
+           ENV.fetch("GOOGLE_CLIENT_ID"),
+           ENV.fetch("GOOGLE_CLIENT_SECRET"),
+           scope: "email,profile"
 end
 
 OmniAuth.config.allowed_request_methods = %i[post]
